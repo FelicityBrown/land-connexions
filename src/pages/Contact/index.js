c35d7d1c41100874657b113269a0f39325f074df
Felicity <felicity@felicitys-air.home>
2021-05-27 15:21:37 +0100

    contact change

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -5,15 +5,14 @@ import { PageHeader } from "./containers/PageHeader";
 import { Footer } from "./components/Footer";
 
 function Contact() {
-    <>
-      <div>
-        <PageHeader />
-        {/* <div className="sm:p-8 sm:mx-12 mt-8 md:mt-4 text-blue-600 items-center flex flex-col text-left text-lg">
+  <>
+    <div>
+      <PageHeader />
+      {/* <div className="sm:p-8 sm:mx-12 mt-8 md:mt-4 text-blue-600 items-center flex flex-col text-left text-lg">
         <div className="max-w-screen-xl md:pl-64 md:pr-8 px-8 md:ml-8"></div> */}
-        <Footer />
-      </div>
-    </>
-  );
+      <Footer />
+    </div>
+  </>;
 }
 
 export default Contact;
